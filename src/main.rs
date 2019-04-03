a026e0bbb8acae150c2cee822c627f7677c927d9
maiso <michielaiso@hotmail.com>
2019-04-03 23:14:54 +0200

    Using the crazy_flie library to blink a led

--- src/main.rs
+++ src/main.rs
@@ -4,39 +4,71 @@
 #![feature(lang_items)]
 extern crate cortex_m_rt;
 
-
 #[panic_handler]
 fn my_panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
-extern crate stm32f4;
-use stm32f4::stm32f405;
-
-// This function is "public to the root" meaning that it's available to external
-// crates linking against this one.
-pub fn init_and_set_bit() 
-{
-	let peripherals = stm32f405::Peripherals::take().unwrap();
-	let gpioc = &peripherals.GPIOC;
-	gpioc.odr.modify(|_, w| w.odr0().set_bit());
-}
-
-// makes `panic!` print messages to the host stderr using semihosting
-//extern crate panic_semihosting;
+extern crate crazy_flie as board;
+extern crate embedded_hal as hal;
 
 use cortex_m_rt::entry;
+
+use board::hal::delay::Delay;
+use board::hal::prelude::*;
+use board::hal::stm32;
+use board::gpio;
+use board::gpio::gpioc::{PC0, PC1, PC2, PC3};
+
+use cortex_m::peripheral::Peripherals;
+
+struct Leds {
+   left_red:    PC0<gpio::Output<gpio::OpenDrain>>,
+   left_green:  PC1<gpio::Output<gpio::OpenDrain>>,
+   right_green: PC2<gpio::Output<gpio::OpenDrain>>,
+   right_red:   PC3<gpio::Output<gpio::OpenDrain>>,
+}
 
 // use `main` as the entry point of this application
 // `main` is not allowed to return
 #[entry]
 fn main() -> ! {
-    // initialization
-    init_and_set_bit();
-    loop {
-        // application logic
-    }
-}
+    if let (Some(p), Some(cp)) = (stm32::Peripherals::take(), Peripherals::take()) {
+        let gpioc = p.GPIOC.split();
 
-#[lang = "eh_personality"]
-extern "C" fn eh_personality() {}
+        // Configure LED outputs
+        let mut leds = Leds {
+          left_red: gpioc.pc0.into_open_drain_output(),
+          left_green: gpioc.pc1.into_open_drain_output(),
+          right_green: gpioc.pc2.into_open_drain_output(),
+          right_red: gpioc.pc3.into_open_drain_output(),
+        };
+
+        // Constrain clock registers
+        let mut rcc = p.RCC.constrain();
+
+        // Configure clock to 168 MHz (i.e. the maximum) and freeze it
+        let clocks = rcc.cfgr.sysclk(168.mhz()).freeze();
+
+        // Get delay provider
+        let mut delay = Delay::new(cp.SYST, clocks);
+
+        loop {
+            // Turn LED on
+            leds.left_red.set_high();
+
+            // Delay twice for half a second due to limited timer resolution
+            delay.delay_ms(500_u16);
+            delay.delay_ms(500_u16);
+
+            // Turn LED off
+            leds.left_red.set_low();
+
+            // Delay twice for half a second due to limited timer resolution
+            delay.delay_ms(500_u16);
+            delay.delay_ms(500_u16);
+        }
+    }
+
+    loop {}
+}
